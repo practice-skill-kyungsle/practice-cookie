bff818d03da49b96f36864e9e8a753b9e9249773
keinn51 <soogood97@gmail.com>
2023-02-24 01:14:21 +0900

    주석 추가해주기

--- server/server.js
+++ server/server.js
@@ -5,20 +5,25 @@ const HTTPS_PORT = 3085;
 
 const app = express();
 
+// client에서 cookie를 받아올 수 있게 해줌
 app.use(cookieParser());
 
 app.use(
     cors({
-        origin: [
-            "http://localhost:5500", // 포트가 달라도 통신을 허용
-        ],
-        credentials: true, // <- 인증정보가 필요한 요청을 허용
+        // 통신을 허용해줄 url 적을 수 있음
+        origin: ["http://localhost:5500"],
+        // 클리이언트가 credentials true인데 얘를 안 켜주면 에러남
+        credentials: true,
     })
 );
 
 app.get("/", (req, res) => {
-    res.cookie("cookieName", "cookieValue");
-    res.status(200).send({ data: "success" });
+    // 클라이언트에 쿠키를 보내줌. key-value 형식
+    res.cookie("cookieKey", "cookieValue");
+    // 클라이언트에서 보내주는 쿠키를 서버에서 받을 수 있음 (cookieParser의 역할)
+    console.log(req.cookies);
+    // response 를 끝내주기.
+    res.end();
 });
 
 app.listen(HTTPS_PORT, () => console.log(`Example app listening on port ${HTTPS_PORT}!`));
