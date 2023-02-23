9d6181e34cb4fec2cbce3804916521021f1c9e01
keinn51 <soogood97@gmail.com>
2023-02-24 00:20:42 +0900

    credential 을 사용해야 쿠키를 보낼 수 있음

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const HTTPS_PORT = 3085;
 
 const app = express();
 
 app.use(cookieParser());
 
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5500", // 포트가 달라도 통신을 허용
+        ],
+        credentials: true, // <- 인증정보가 필요한 요청을 허용
+    })
+);
+
 app.get("/", (req, res) => {
-    res.setHeader("Access-Control-Allow-origin", "*");
+    console.log("res!");
+    // res.setHeader("Access-Control-Allow-origin", "*");
 
     // 이렇게 하면 클라이언트에 심어짐?
     res.cookie("cookieName", "cookieValue");
-    // console.log(res);
     res.status(200).send({ data: "success" });
 });
 
